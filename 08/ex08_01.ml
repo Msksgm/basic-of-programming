d48a04149ddcbc61964a98fbdc2db9e5cd0ba0f3
Msksgm <msksgm3124@gmail.com>
2022-05-01 07:24:30 +0900

    add: ex08_01

---
+++ 08/ex08_01.ml
@@ -0,0 +1,10 @@
+type book_t = {
+    taitoru: string;
+    chosya: string;
+    syuppansya: string;
+    nedan: int;
+    isbn: string;
+}
+{taitoru="taitoru"; chosya="choysa"; syuppansya="syuppansya"; nedan=100; isbn="isbn"}
+{taitoru="taitoru1"; chosya="choysa1"; syuppansya="syuppansya1"; nedan=200; isbn="isbn1"}
+{taitoru="taitoru2"; chosya="choysa2"; syuppansya="syuppansya2"; nedan=300; isbn="isbn2"}
